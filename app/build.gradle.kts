801f31dfc93e890aae16e1c3600ceda1eb882c1d
Cheuk Yin Ng <chucksys88@gmail.com>
2026-03-27 00:26:47 -0700

    chore: version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "ca.cheuksblog.scrabblechecker"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "2.1"
+        versionCode = 4
+        versionName = "2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
